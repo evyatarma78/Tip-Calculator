97c4f1f3aa7f626c78b7d24c82ba6814499d2a6a
evyatarma78 <85066689+evyatarma78@users.noreply.github.com>
2022-10-07 14:10:31 +0300

    Add files via upload

--- source.js
+++ source.js
@@ -2,7 +2,7 @@ function calc() {
   var amount = document.getElementById("amount").value;
   var per = document.getElementById("percent").value;
   var persons = document.getElementById("persons").value;
-  let tip = Math.round((amount * per) / 100);
+  let tip = ((amount * per) / 100);
 
   if (amount == "") {
     alert("אנא מלא את סכום הארוחה");
@@ -22,9 +22,13 @@ function calc() {
     document.getElementById("tip").innerHTML =
       " סכום הטיפ הינו " + "<br>" + tip + "&nbsp" + " ש״ח";
 
-    let f_bill = Math.round((+amount + +tip) / persons);
+    let f_bill =((+amount + +tip) / persons);
+
+    tab = (f_bill * persons).toFixed(1);
 
     document.getElementById("finel").innerHTML =
       "הסכום לתשלום לכל סועד  הינו" + "&nbsp" + f_bill + "&nbsp" + "ש״ח";
+
+      document.getElementById("tab").innerHTML =  'סה"כ לתשלום ' + "&nbsp" + tab + "&nbsp" + "ש״ח";
   }
 }
