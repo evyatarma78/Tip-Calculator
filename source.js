f076ea3b0dd9af242e9ae1701021ed32d62d1b2b
evyatarma78 <85066689+evyatarma78@users.noreply.github.com>
2022-11-24 20:06:47 +0200

    Add files via upload

--- source.js
+++ source.js
@@ -1,41 +1,36 @@
-function calc() {
-  var amount = document.getElementById("amount").value;
-  var per = document.getElementById("percent").value;
-  var persons = document.getElementById("persons").value;
-  let tip = ((amount * per) / 100);
+const para = document.querySelector("#result > p");
+let txt = "";
+const printToResult = (key)=>{
+    if (txt == 0) {
+        txt = "";
+        txt += key;
+        para.innerHTML = txt;
+    }
 
-  if (amount == "" ||amount == NaN || amount <=0) {
-    alert("אנא מלא את סכום הארוחה");
-    return;
-  } 
-  else if (persons == "" || persons <=0) {
-    alert(" אנא השלם את כמות הסועדים");
-    return;
-
-  } else if (tip == "" ||tip <=0) {
-    alert("אנא השלם את אחוז הטיפ");
-    return;
-
-  } 
-  else {
-   
-    document.getElementById("tip").innerHTML =
-      " סכום הטיפ הינו " + "<br>" + tip + "&nbsp" + " ש״ח";
-
-    let f_bill =((+amount + +tip) / persons);
-
-    tab = (f_bill * persons).toFixed(1);
-
-    document.getElementById("finel").innerHTML =
-      "הסכום לתשלום לכל סועד  הינו" + "&nbsp" + f_bill.toFixed(1) + "&nbsp" + "ש״ח";
-
-      document.getElementById("tab").innerHTML =  'סה"כ לתשלום ' + "&nbsp" + tab + "&nbsp" + "ש״ח";
-
-       document.getElementById("hide").style.display = "inherit"
-
-  }
+    else {
+        txt += key;
+        para.innerHTML = txt;
+    }
 
 }
-let d = new Date();
 
-document.getElementById("para").innerHTML = "Evyatar Mahatsri ©" + d.getFullYear();
+const showResult = () =>{
+    
+    
+    
+    if (typeof eval(txt)) {
+        let final = eval(txt);
+        txt = final;
+        para.innerHTML = final;
+    }
+
+    else {
+        para.innerHTML = "ERROR";
+    }
+
+}
+
+const clearResult = () => {
+    txt = "0";
+    para.innerHTML = txt;
+}
